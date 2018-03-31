7e71d48a0d9d3dcd78d0400ce0fa73038bb1da94
natalie <nailiqi@gmail.com>
2018-03-31 00:07:33 -0400

    customize navbar

--- app/src/main/java/com/nailiqi/travellingpaws/MainActivity.java
+++ app/src/main/java/com/nailiqi/travellingpaws/MainActivity.java
@@ -1,13 +1,25 @@
 package com.nailiqi.travellingpaws;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void setupBottomNavbar() {
+        Log.d(TAG, "setupBottomNavbar: Setting up Bottom navigation bar");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavbar);
+
     }
 }
