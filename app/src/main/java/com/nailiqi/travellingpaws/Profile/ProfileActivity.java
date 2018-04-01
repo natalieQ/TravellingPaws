347ac4954d0c14beee4116e558911c4767ff50fc
natalie <nailiqi@gmail.com>
2018-04-01 00:44:54 -0400

    add profile activity-layout

--- app/src/main/java/com/nailiqi/travellingpaws/Profile/ProfileActivity.java
+++ app/src/main/java/com/nailiqi/travellingpaws/Profile/ProfileActivity.java
@@ -3,6 +3,7 @@ package com.nailiqi.travellingpaws.Profile;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,10 +19,33 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: starting");
 
         setupBottomNavbar();
+        setupTopToolbar();
+    }
+
+    /**
+     * Top Toolbar setup
+     */
+    private void setupTopToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
+        //set toolbar as action bar
+        setSupportActionBar(toolbar);
+        
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Log.d(TAG, "onMenuItemClick: menu item clicked: " + item);
+                
+                switch (item.getItemId()){
+                    case R.id.profileMenu:
+                        Log.d(TAG, "onMenuItemClick: go to profile edit");
+                }
+                return false;
+            }
+        });
     }
 
     /**
@@ -38,5 +62,11 @@ public class ProfileActivity extends AppCompatActivity {
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
     }
 }
