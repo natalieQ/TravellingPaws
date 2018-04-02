ead066300f6820f5eb914ef2016e06a20d35e215
natalie <nailiqi@gmail.com>
2018-04-02 02:33:52 -0400

    add signout route

--- app/src/main/java/com/nailiqi/travellingpaws/Profile/SignOutFragment.java
+++ app/src/main/java/com/nailiqi/travellingpaws/Profile/SignOutFragment.java
@@ -1,24 +1,89 @@
 package com.nailiqi.travellingpaws.Profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.nailiqi.travellingpaws.R;
+import com.nailiqi.travellingpaws.Signin.SigninActivity;
 
 public class SignOutFragment extends Fragment{
 
     private static final String TAG = "SignOutFragment";
+    //firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private Button btnSignout;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signout, container, false);
+        btnSignout = (Button) view.findViewById(R.id.btn_signout);
+
+        setupFirebaseAuth();
+
+        btnSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                getActivity().finish();
+            }
+        });
 
         return view;
+    }
+
+    /**
+     * Setup the firebase auth
+     */
+    private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth.");
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+
+                    Log.d(TAG, "onAuthStateChanged: go to signin activity");
+                    Intent intent = new Intent(getActivity(), SigninActivity.class);
+                    startActivity(intent);
+                }
+                // ...
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
