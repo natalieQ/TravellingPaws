84a77599debb9837f128e55502e98fef94916b1b
natalie <nailiqi@gmail.com>
2018-04-08 22:37:54 -0400

    add check permissions in shareActivity

--- app/src/main/java/com/nailiqi/travellingpaws/Share/ShareActivity.java
+++ app/src/main/java/com/nailiqi/travellingpaws/Share/ShareActivity.java
@@ -1,7 +1,9 @@
 package com.nailiqi.travellingpaws.Share;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -10,10 +12,12 @@ import android.view.MenuItem;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.nailiqi.travellingpaws.R;
 import com.nailiqi.travellingpaws.Utils.BottomNavbarHelper;
+import com.nailiqi.travellingpaws.Utils.Permissions;
 
 public class ShareActivity extends AppCompatActivity {
     private static final String TAG = "ShareActivity";
     private static final int ACTIVITY_NUM = 2;
+    private static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,7 +25,48 @@ public class ShareActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: starting");
 
+        if(checkPermissionArray(Permissions.PERMISSIONS)){
+
+        } else {
+            verifyPermissions(Permissions.PERMISSIONS);
+        }
+
         setupBottomNavbar();
+    }
+
+    public boolean checkPermissionArray(String[] permissions){
+        Log.d(TAG, "checkPermissionArray: check permission array");
+
+        for(int i = 0; i< permissions.length; i++){
+            if(!checkPermissions(permissions[i])){
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public void verifyPermissions(String[] permissions){
+        ActivityCompat.requestPermissions(
+                ShareActivity.this,
+                permissions,
+                REQUEST_CODE
+        );
+    }
+
+    public boolean checkPermissions(String permission){
+        Log.d(TAG, "checkPermissions: checking permission: " + permission);
+
+        int request = ActivityCompat.checkSelfPermission(ShareActivity.this, permission);
+
+        if(request != PackageManager.PERMISSION_GRANTED){
+            Log.d(TAG, "checkPermissions: Permission was not granted for: " + permission);
+            return false;
+        }
+        else{
+            Log.d(TAG, "checkPermissions: Permission was granted for: " + permission);
+            return true;
+        }
     }
 
     /**
