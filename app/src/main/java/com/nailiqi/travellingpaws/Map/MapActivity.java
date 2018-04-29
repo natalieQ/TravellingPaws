48501bf12da78fe908b5fce13cfd75f07e3dad42
natalie <nailiqi@gmail.com>
2018-04-28 21:59:37 -0400

    add google map to mapActivity

--- app/src/main/java/com/nailiqi/travellingpaws/Map/MapActivity.java
+++ app/src/main/java/com/nailiqi/travellingpaws/Map/MapActivity.java
@@ -1,8 +1,12 @@
 package com.nailiqi.travellingpaws.Map;
 
 import android.app.Dialog;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -11,31 +15,107 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.nailiqi.travellingpaws.Home.MainActivity;
+import com.nailiqi.travellingpaws.Manifest;
 import com.nailiqi.travellingpaws.R;
 import com.nailiqi.travellingpaws.Utils.BottomNavbarHelper;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady: showing map now");
+        mMap = googleMap;
+    }
+
     private static final String TAG = "MapActivity";
     private static final int ACTIVITY_NUM = 1;
     private static final int ERROR_DIALOG_REQUEST = 9001;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 10;
+
+    //permissions
+    private static final String FINE_LOCATION = android.Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = android.Manifest.permission.ACCESS_COARSE_LOCATION;
+
+    private boolean mLocationPermissionGranted = false;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_map);
         Log.d(TAG, "onCreate: starting");
 
         setupBottomNavbar();
 
         if(checkServices()){
-            init();
+            getLocationPermission();
         }
     }
 
-    private void init(){
 
+    /**
+     * initialize map
+     */
+    private void initMap() {
+        Log.d(TAG, "initMap: init map now");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(MapActivity.this);
+    }
+
+
+    /**
+     * check location permissions
+     */
+    private void getLocationPermission(){
+        Log.d(TAG, "getLocationPermission: getting location permissions");
+        String[] permissions = {android.Manifest.permission.ACCESS_FINE_LOCATION,
+                                android.Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "getLocationPermission: permission granted");
+                mLocationPermissionGranted = true;
+
+            } else {
+                //request permissions
+                ActivityCompat.requestPermissions( this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            //request permissions
+            ActivityCompat.requestPermissions( this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: request permission");
+        mLocationPermissionGranted = false;
+
+        switch(requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE : {
+                if(grantResults.length > 0 ) {
+                    for(int i=0; i<grantResults.length; i++){
+                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            mLocationPermissionGranted = false;
+                            return;
+                        }
+                    }
+                    Log.d(TAG, "onRequestPermissionsResult: permission granted");
+                    mLocationPermissionGranted = true;
+
+                    //init google map
+                    initMap();
+                }
+            }
+        }
     }
 
     /**
@@ -75,4 +155,6 @@ public class MapActivity extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
     }
+
+
 }
